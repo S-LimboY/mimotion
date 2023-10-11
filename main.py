e3ac920c8904217bb92ab566eb6ee8ef40a5b17e
EDEN <53947518+S-LimboY@users.noreply.github.com>
2023-10-11 21:51:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def main(_user, _passwd, _step):
         return "user and passwd not empty！"
 
     if _step == '':
-        print("已设置为随机步数（6000-10999）")
-        _step = str(random.randint(6000, 10999))
+        print("已设置为随机步数（9000-12999）")
+        _step = str(random.randint(9000, 12999))
     login_token, userid = login(_user, password)
     if login_token == 0:
         print("登陆失败！")
